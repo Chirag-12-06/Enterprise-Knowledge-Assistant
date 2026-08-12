db08e843fbc8b459f00a27a32b00343f24edd79e
Chirag Gupta <chirag.gupta.2120@gmail.com>
2026-08-12 17:50:56 +0530

    final changes

--- backend/src/services/embedding.service.js
+++ backend/src/services/embedding.service.js
@@ -1,7 +1,5 @@
 const axios = require("axios");
 
-const EMBEDDING_API = "http://127.0.0.1:8000/embed";
-
 async function generateEmbeddings(texts) {
     const response = await axios.post(EMBEDDING_API, {
         texts,
